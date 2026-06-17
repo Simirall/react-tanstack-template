3533ddc09d5f1e6ab1003e68c2c3d610a2bd897c
Simirall <37952374+Simirall@users.noreply.github.com>
2026-06-18 01:52:52 +0900

    tanstack router & query

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,29 @@
-export const App = () => {
-  return <>React TanStack Template</>;
-};
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { createRouter, RouterProvider } from "@tanstack/react-router";
+import { routeTree } from "./routeTree.gen.ts";
+
+const queryClient = new QueryClient();
+
+export type RouterContext = { queryClient: QueryClient };
+
+const router = createRouter({
+  context: {
+    queryClient: queryClient,
+  } satisfies RouterContext,
+  defaultPreload: "intent",
+  routeTree,
+});
+
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+export function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  );
+}
